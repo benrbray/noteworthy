5254a12935dd6443b23b491f4f4788a8eae66fe6
Benjamin R. Bray <benrbray@gmail.com>
2020-06-05 13:27:18 -0400

    augment with missing types

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,3 +3,10 @@ declare module NodeJS {
 		isQuitting?: boolean;
 	}
 }
+
+// convenience type for common prosemirror callback signature
+type ProseCommand = (
+	state: import("prosemirror-state").EditorState,
+	dispatch?: ((tr: import("prosemirror-state").Transaction) => void),
+	view?: import("prosemirror-view").EditorView
+) => boolean;
