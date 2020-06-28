b9636fbda37f865b43e8f3bbeb5652754ed9d64d
Benjamin R. Bray <benrbray@gmail.com>
2020-06-28 12:10:17 -0400

    replace java hash with cyrb53

--- src/common/util/hash.ts
+++ src/common/util/hash.ts
@@ -22,21 +22,18 @@ import * as pathlib from "path";
  */
 
 /**
-* Basic hashing function (thanks to https://stackoverflow.com/a/7616484)
+* Basic hashing function (thanks to https://stackoverflow.com/a/52171480/1444650)
 * @param  str The string that should be hashed
 * @return hash of the given string
 */
-export default function hash(str:string):FileHash {
-	str = pathlib.normalize(str);
-	let hash = 0
-	let i, chr
-
-	if (str.length === 0) return hash.toString();
-
-	for (i = 0; i < str.length; i++) {
-		chr = str.charCodeAt(i)
-		hash = ((hash << 5) - hash) + chr
-		hash |= 0 // Convert to 32bit integer
+export default function hash(str:string, seed:number = 0):FileHash {
+	let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
 	}
-	return hash.toString();
-}
+	h1 = Math.imul(h1 ^ h1 >>> 16, 2246822507) ^ Math.imul(h2 ^ h2 >>> 13, 3266489909);
+	h2 = Math.imul(h2 ^ h2 >>> 16, 2246822507) ^ Math.imul(h1 ^ h1 >>> 13, 3266489909);
+	return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString();
+};
