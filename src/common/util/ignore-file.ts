b01bf0e31581e63f4c6b3d657cef7dc771cbef2d
Benjamin R. Bray <benrbray@gmail.com>
2020-07-02 11:22:36 -0400

    allow .nwt and .bib files

--- src/common/util/ignore-file.ts
+++ src/common/util/ignore-file.ts
@@ -3,14 +3,14 @@ import pathlib from "path";
 
 // Supported filetypes
 /** @todo (6/28/20) read supported file types from config file? */
-const filetypes = [".md", ".txt", ".ipynb", ".journal"]
+const filetypes = [".md", ".txt", ".ipynb", ".journal", ".nwt", ".bib"];
 
 /**
 * Returns true, if a given file should be ignored.
-* @param  {String} p The path to the file.
-* @return {Boolean}   True or false, depending on whether the file should be ignored.
+* @param  p The path to the file.
+* @return Whether the file should be ignored.
 */
-export function ignoreFile(p:string) {
+export function ignoreFile(p:string):boolean {
 	let ext = pathlib.extname(p).toLowerCase()
 	return (!filetypes.includes(ext))
 }
