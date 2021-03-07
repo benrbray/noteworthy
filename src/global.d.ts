4e5a79029e1dc2c1968491265bed9697291bebce
Benjamin Bray <benrbray@gmail.com>
2021-03-08 01:21:32 +0900

    re-introduce global.isQuitting

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,11 @@
 // type declarations
 
+declare module NodeJS {
+	declare interface Global {
+		isQuitting: boolean;
+	}
+}
+
 interface ProxyConstructor {
 	new <TSource extends object, TTarget extends object>(target: TSource, handler: ProxyHandler<TSource>): TTarget;
 }
