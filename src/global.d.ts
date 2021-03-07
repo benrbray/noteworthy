30fed0006b762a3bc2d0b90e79af63b26819700a
Benjamin Bray <benrbray@gmail.com>
2021-03-08 01:02:56 +0900

    remove unused global declarations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,52 +1,5 @@
 // type declarations
 
-interface ILogger {
-	verbose: ((msg: string, details?: any) => void);
-	info: ((msg: string, details?: any) => void);
-	warning: ((msg: string, details?: any) => void);
-	error: ((msg: string, details?: any) => void);
-	showLogViewer: (() => void);
-}
-
-interface ITagManager {
-	report: (tagArray: string[]) => void;
-	remove: (tagArray: string[]) => void;
-	getTagDatabase: () => Object;
-	getSpecialTags: (name: string) => Tag|undefined;
-	getAllSpecialTags: () => Tag[];
-	update: (newTags: Tag[]) => boolean;
-}
-
-interface IAppManager {
-	isBooting: () => boolean;
-	/** @todo: (6/6/20) fill in these functions? */
-	fileUpdate: (oldHash:string, fileMetadata:IFileMetadata) => void;
-	dirUpdate:(oldHash:string, newHash:string) => void;
-	notifyChange:(msg:string)=>void;
-	findFile:(prop)=>File;
-	findDir:(prop)=>File;
-	getFile: (fileDesc:IFileDesc)=>IFileWithContents;
-}
-
-interface IWatchdog {
-	addPath: (p:string) => void;
-	ignoreNext: (event, path:string) => void;
-	stop: () => void;
-	on: (event, callback:((...args:any[])=>void)) => void;
-	off: (event, callback: ((...args: any[]) => void)) => void;
-}
-
-/** @todo (9/13/20) remove as many globals as possible */
-declare namespace NodeJS {
-	interface Global {
-		isQuitting?: boolean;
-		log: ILogger;
-		tags:ITagManager;
-		application:IAppManager;
-		watchdog:IWatchdog;
-	}
-}
-
 interface ProxyConstructor {
 	new <TSource extends object, TTarget extends object>(target: TSource, handler: ProxyHandler<TSource>): TTarget;
 }
