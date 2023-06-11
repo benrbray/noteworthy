1c713b85b8fe95253e0b851aaa82e32ca04466b6
Benjamin Bray <benrbray@gmail.com>
2023-06-11 09:00:41 +0900

    rename main.ts -> index.ts

--- noteworthy-editor/vite.config.ts
+++ noteworthy-editor/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'NoteworthyEditor',
       fileName: 'noteworthy-editor',
     },
